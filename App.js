7ce851ecee43b14439658f2dbb5bbf267bda4628
Milzam Hibatullah <hmilzam@gmail.com>
2019-06-13 15:40:25 +0700

    use scaling to create responsive apps

--- App.js
+++ App.js
@@ -1,49 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React  from  'react';
+import  {
+  View,
+  Text,
+  Image,
+  Dimensions
+} from  'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const {width,height}  = Dimensions.get('window');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+//guideline sizes on standar 5"
+const guidelineBaseWidth  = 350;
+const guidelineBaseHeight = 680;
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+const scale = size  =>  width / guidelineBaseWidth  * size;
+const verticalScale = size  =>  height  / guidelineBaseHeight * size;
+const moderateScale = (size,  factor  = 0.5)  =>  size  + ( scale(size) - size) * factor;
+
+export  default class App extends React.Component{
+  render(){
+    return(
+      <View style={{backgroundColor:'white',  flex:1, alignItems:'center',  justifyContent:'center'}}>
+          <Text style={{fontSize:moderateScale(18),color:'#000'}}>Hello Word</Text>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
